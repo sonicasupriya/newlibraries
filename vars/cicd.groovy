813be4f094fdf96194d6d0b6d16a3b99bc1d9062
sonicasupriya <140101428+sonicasupriya@users.noreply.github.com>
2023-08-03 20:09:48 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,20 @@
-  
+def gitDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
+
+def mavenBuild()
+{
+  sh 'mvn package'
+}
+
+def tomcatDeploy(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def runSelinium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+ 
